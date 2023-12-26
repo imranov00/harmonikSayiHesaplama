f5f84d085b37745cd50d9221cca7ed28ae7cabe4
İmranov <semtexpert@gmail.com>
2023-12-26 16:11:40 +0300

    Version v1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inp = new Scanner(System.in);
+        System.out.println("N sayısını Giriniz:  ");
+        int n = inp.nextInt();
+        double result = 0;
+
+
+
+        for (double i = 1; i <=n; i ++){
+
+            result += (1/i);
+
+
+        }
+        // 1. Döngü, i = 1; result = 0 + (1/1)=1;
+        // 2. Döngü ,i = 2; result = 1 + (1/2)=1.5
+        System.out.println("Harmonik Sonuç: " + result);
+
     }
 }
